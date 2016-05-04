3bb31e464838e5bd6de09983640b1562870a17a0
kM <chamajiuxi@126.com>
2016-05-04 18:26:58 -0400

    add "all","active","finished" selection

--- components/App.vue
+++ components/App.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="app">
+    <header>
+      <h1>Todos</h1>
+      <sub>({{selectedFilter}})</sub>
+    </header>
     <div class="input-wrap">
         <input class="input-todo" type="text" placeholder="输入todo" maxlength="10" autofocus @keyup.enter="tryAddTodo">
     </div>
     <ul class="todo-list">
-      <todo v-for="todo in todos" transition="animate" :todo="todo"></todo>
+      <todo v-for="todo in filteredTodos" transition="animate" :todo="todo"></todo>
     </ul>
+    <footer>
+      <ul>
+        <li v-for="(key, value) in filters">
+          <a href="javascript: void(0);" @click="selectedFilter=key">
+            {{capitalize(key)}}
+          </a>
+        </li>
+      </ul>
+    </footer>
   </div>
 </template>
 
@@ -15,9 +28,26 @@
   import {addTodo} from "../vuex/actions.js";
   import {todos} from "../vuex/getters.js";
 
+  const filters = {
+    "all": todos => todos,
+    "active": todos => todos.filter(todo => !todo.done),
+    "finished": todos => todos.filter(todo => todo.done)
+  };
+
   export default {
     components: {
       Todo
+    },
+    data() {
+      return {
+        selectedFilter: "all",
+        filters
+      }
+    },
+    computed: {
+      filteredTodos() {
+        return this.filters[this.selectedFilter](this.todos);
+      }
     },
     store,
     methods: {
@@ -30,6 +60,9 @@
           });
           e.target.value = "";
         }
+      },
+      capitalize(key) {
+        return key.replace(/^\w/, letter => letter.toUpperCase());
       }
     },
     vuex: {
@@ -44,30 +77,66 @@
 </script>
 
 <style>
+input:focus {
+  outline: none;
+}
+header {
+  width: 15em;
+  margin: 0 auto;
+  text-align: center;
+  color: #eb8888;
+  padding: 20px 0;
+  background: #ffefee;
+}
 .todo-list {
   list-style-type: none;
   padding-left: 0px;
-  width: 10em;
+  width: 15em;
   margin: 0 auto;
 }
 .input-wrap {
-  width: 10em;
+  width: 15em;
   margin: 0 auto;
 }
 .input-todo {
+  font-size: 1.2em;
   width: 100%;
   border: none;
-  border-bottom: 1px solid;
+  border-bottom: 1px solid #aaa;
+  box-shadow: inset 0 -2px 1px rgba(0,0,0,0.05);
   text-align: center;
+  padding: 10px 0;
 }
 .animate-transition {
+  padding: 0.4em;
   transition: all 0.5s ease;
-  height: 10px;
-  padding: 5px;
 }
 .animate-enter, .animate-leave {
-  height: 0;
-  padding: 0 5px;
+  padding: 0.4em 0 0.4em 2em;
   opacity: 0;
+}
+footer {
+  width: 15em;
+  margin: 0 auto;
+}
+footer ul {
+  padding: 0;
+  text-align: center;
+}
+footer li {
+  list-style: none;
+  display: inline-block;
+  margin: 5px 10px;
+  border: 1px #eb8888 solid;
+  border-radius: 0.5em;
+  padding: 2px 5px;
+  transition: all 0.5s ease;
+}
+footer li a {
+  text-decoration: none;
+  color: #eb8888;
+}
+footer li:hover {
+  background-color: #ffefee;
 }
 </style>
--- components/Todo.vue
+++ components/Todo.vue
@@ -1,8 +1,10 @@
 <template>
-  <li>
-    <input class="toggle-todo" type="checkbox" :checked="todo.done" @change="toggleTodo(todo)" :class="{'done': todo.done}">
-    <span class="value-todo">{{ todo.value }}</span>
-    <input type="button" value="x" @click="deleteTodo(todo)">
+  <li class="todo">
+    <div class="wrap-todo">
+      <input class="toggle-todo" type="checkbox" :checked="todo.done" @change="toggleTodo(todo)" :class="{'done': todo.done}">
+      <span class="value-todo">{{ todo.value }}</span>
+    </div>
+    <input class="delete-todo" type="button" value="x" @click="deleteTodo(todo)">
   </li>
 </template>
 
@@ -23,11 +25,39 @@
 </script>
 
 <style>
+.todo {
+  border-bottom: 1px solid #aaa;
+  overflow: hidden;
+  position: relative;
+  /*padding: 50px;*/
+}
+.wrap-todo {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+}
 .value-todo {
+  line-height: 1.5em;
   transition: all 0.5s ease;
+  color: #333;
 }
 .toggle-todo.done + .value-todo {
   text-decoration: line-through;
   color: grey;
+}
+.delete-todo {
+  visibility: hidden;
+  /*padding: 0.2em;*/
+  font-size: 1.6em;
+  float: right;
+  border: none;
+  background-color: transparent;
+  line-height: 1.5em;
+  color: #eb8888;
+}
+.todo:hover .delete-todo {
+  visibility: visible;
 }
 </style>
